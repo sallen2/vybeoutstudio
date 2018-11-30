43a055ba1041588503f2b9c45e4e0fccf396d431
Stephan Allen <sallen202009@gmail.com>
2018-11-30 01:19:58 -0500

    minor change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const home = require('./controllers/vybeOut');
+const vybeOut = require('./controllers/vybeOut');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,6 +9,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use('/', home);
+app.use('/', vybeOut);
 
 app.listen(process.env.PORT || 8080, () => console.log('server up on port 8080'));
