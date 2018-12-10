ee2d74bb06601b2d1697f68bc0d7ca85ba145a80
Stephan Allen <sallen202009@gmail.com>
2018-12-10 02:11:18 -0500

    done

--- public/js/contribute.js
+++ public/js/contribute.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  $('a').on('click', function (e) {
+  $('.contribute').on('click', function (e) {
     localStorage.setItem('Producer', $(this).attr('producer'));
     localStorage.setItem('Beat', $(this).attr('beat'));
     localStorage.setItem('Tempo', $(this).attr('tempo'));
